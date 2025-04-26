a2fb1ffa6ce2be8fd84a13855b3f31aaa5b4b487
Kwan <nkhoi010305@gmail.com>
2025-04-26 22:18:35 +0700

    adding rot13 to cryptogg

--- cryptogg.py
+++ cryptogg.py
@@ -1,1 +1,31 @@
-print("Hello world!")
+
+
+"""
+    A fucking rot13 machine
+    input: a string
+    output: a rotated string
+"""
+
+def rot13(text):
+    result = []
+    for char in text:
+        ascii_val = ord(char) # turn char to ascii value. Ex: A -> 65
+        print(ascii_val)
+
+        if "A" <= char <= "Z":
+            new_ascii = ascii_val + 13 # (65 - 65 + 13) % 26 + 65 = 78 -> which is N in ascii
+            result.append(chr(new_ascii))
+
+        elif "a" <= char <= 'z':
+            new_ascii = ascii_val + 13
+            result.append(chr(new_ascii))
+
+        else:
+            result.append(char)
+
+    jointed_result = "".join(result)
+    print(jointed_result)
+
+rot13('a')
+
+
